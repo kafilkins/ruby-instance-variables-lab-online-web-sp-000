deed70632f353363bd7341a0b54d82551867b4a1
Keith Filkins <kafilkins@me.com>
2021-01-02 22:01:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,6 @@
 class Dog
-  def name= (dog_name)
+
+  def name=(dog_name)
     @this_dogs_name = dog_name
   end
 
@@ -7,8 +8,3 @@ class Dog
     @this_dogs_name
   end
 end
-
-lassie = Dog.new
-lassie.name = "Lassie"
-
-puts lassie.name
